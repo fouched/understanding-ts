70b61d275afb4f398c242db9d8d7765aae9e3775
Fouche du Preez <fouche@limehouse.co.za>
2022-04-19 20:25:54 +0200

    ongoign

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
-interface Person {
-	name: string
-	age: number
-
+interface Greetable {
 	greet(phrase: string) : void
 }
+
+class Person implements Greetable {
+
+	constructor(private name: string, private age: number) {
+	}
+
+	greet(phrase: string): void {
+		console.log(phrase + ' ' + this.name)
+	}	
+}
+
+let user1 = new Person('Fouche', 50)
+user1.greet('Hi')
